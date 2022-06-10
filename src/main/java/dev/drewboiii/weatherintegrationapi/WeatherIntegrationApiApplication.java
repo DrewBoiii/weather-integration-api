85c43295b0ecab438df867e10b202ea1292333be
DrewBoiii <dhdhdh06@mail.ru>
2022-06-10 21:10:36 +0400

    init

---
+++ src/main/java/dev/drewboiii/weatherintegrationapi/WeatherIntegrationApiApplication.java
@@ -0,0 +1,13 @@
+package dev.drewboiii.weatherintegrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherIntegrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherIntegrationApiApplication.class, args);
+	}
+
+}
