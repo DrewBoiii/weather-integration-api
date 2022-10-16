1ada6ff8bde5106734f7b0153dc307c4a1d1b833
drewboiii <goodchickenboy@mail.ru>
2022-10-16 22:25:59 +0400

    self inject

--- src/main/java/dev/drewboiii/weatherintegrationapi/schedule/TableCleanerSchedule.java
+++ src/main/java/dev/drewboiii/weatherintegrationapi/schedule/TableCleanerSchedule.java
@@ -3,9 +3,11 @@ package dev.drewboiii.weatherintegrationapi.schedule;
 import dev.drewboiii.weatherintegrationapi.service.RequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.time.Period;
 
 @Component
@@ -17,9 +19,17 @@ public class TableCleanerSchedule {
 
     private final RequestService requestService;
 
+    private final ApplicationContext applicationContext;
+    private TableCleanerSchedule self;
+
     @Scheduled(cron = "${schedule.clean-table.request.cron:-}")
     public void cleanRequestTable() {
         requestService.deleteRequestsExpiredBy(interval);
+    }
+
+    @PostConstruct
+    private void init() {
+        this.self = applicationContext.getBean(TableCleanerSchedule.class);
     }
 
 }
