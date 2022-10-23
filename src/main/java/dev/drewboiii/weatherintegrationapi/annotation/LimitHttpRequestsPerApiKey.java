2d9e8505284919969b04aefe8b083ddfc0290868
drewboiii <goodchickenboy@mail.ru>
2022-10-23 22:22:00 +0400

    minor improvements

--- src/main/java/dev/drewboiii/weatherintegrationapi/annotation/LimitHttpRequestsPerApiKey.java
+++ src/main/java/dev/drewboiii/weatherintegrationapi/annotation/LimitHttpRequestsPerApiKey.java
@@ -1,4 +1,4 @@
-package dev.drewboiii.weatherintegrationapi.aspect;
+package dev.drewboiii.weatherintegrationapi.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
